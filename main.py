cca67c99909c9176511ad4088652d31f2b196e41
Mark <an.mark96@gmail.com>
2018-09-22 03:47:23 +0300

    FINISH

--- main.py
+++ main.py
@@ -26,9 +26,7 @@ def login():
 
 @app.route('/authorize', methods=['GET'])
 def authorize():
-    if c.STATE == request.args.get('state'):
+    if "state" in request.args == c.STATE:
         token = requests.get(c.access_url(request.args.get('code'))).json()['access_token']
         return app.make_response(redirect('/')).set_cookie('token', value=token,
                                                            expires=datetime.datetime.now() + datetime.timedelta(days=1))
-    else:
-        return request.args.get('state')
