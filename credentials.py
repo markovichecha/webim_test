ebe5c593206119607703b12e312fb2a47fca9bef
Mark <an.mark96@gmail.com>
2018-09-22 03:43:54 +0300

    FINISH

--- credentials.py
+++ credentials.py
@@ -9,7 +9,7 @@ SCOPE = "friends, users"
 V = "5.85"
 FIELDS = "photo, name"
 ORDER = "random"
-STATE = 389730183
+STATE = "389730183"
 COUNT = "5"
 
 PARAMETERS_CODE = {'client_id': CLIENT_ID,
--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if 'token' in request.cookies:
         token = request.cookies.get('token')
-        r = requests.get(c.get_name(token)).json()["responce"]
-        name = r["first_name"] + " " + r["last_name"]
+        r = requests.get(c.get_name(token)).json()['responce']
+        name = r['first_name'] + " " + r['last_name']
         return render_template('list.html', name=name)
     else:
         return render_template('index.html')
@@ -20,15 +20,15 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == "POST":
+    if request.method == 'POST':
         return redirect(c.code_url())
 
 
 @app.route('/authorize', methods=['GET'])
 def authorize():
-    if request.args.get("state") == 389730183:
-        token = requests.get(c.access_url(request.args.get("code"))).json()["access_token"]
+    if c.STATE == request.args.get('state'):
+        token = requests.get(c.access_url(request.args.get('code'))).json()['access_token']
         return app.make_response(redirect('/')).set_cookie('token', value=token,
                                                            expires=datetime.datetime.now() + datetime.timedelta(days=1))
     else:
-        return request.args.get("state")
+        return request.args.get('state')
